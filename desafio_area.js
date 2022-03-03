91ec7a91f5d778438ab1e6d887b72717db8542c4
FelipeCabralViana <felipecv7@gmail.com>
2022-03-03 18:15:06 -0300

    Commit 3

---
+++ desafio_area.js
@@ -0,0 +1,18 @@
+// PI * raio * raio 
+
+
+const raio = 10;
+const PI = 3.14;
+let areaCir = PI * raio * raio;
+
+console.log("O valor da área é: "
+     + areaCir + " m²");
+
+
+console.log(Math.PI);
+
+areaCir = Math.PI * raio * raio;
+console.log("O valor da área é: "
+     + areaCir + " m²");
+
+
---
+++ desafio_troca.js
@@ -0,0 +1,16 @@
+let a = 7;
+let b = 94;
+
+let c = 0;
+
+
+//depois da troca.. a = 94 e b = 7
+
+
+c = a;
+a = b;
+b = c;
+
+console.log("Valor de a: " + a);
+console.log("Valor de b: " + b);
+
