6fa0b971dbc8d1e3a996035bbd0a079b2c08b5e0
FelipeCabralViana <felipecv7@gmail.com>
2022-03-04 18:48:19 -0300

    Commit 5 - 5

---
+++ exercicios-js/Javascript/fundamentos_2/objetos.js
@@ -0,0 +1,38 @@
+const prod1 = {};
+//{} forma literal de criar um objeto
+
+// Coleção de chaves e valores
+prod1.nome = 'celular ultra mega';
+prod1.preco = 1998.90;
+prod1['desconto legal'] = 0.40; //evitar atributos com espaço
+
+console.log(prod1);
+
+
+const prod2 = {
+
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj: {
+
+        blabla: 1,
+        obj: {
+
+            blabla: 2
+
+        }
+
+    }
+}
+
+console.log(prod2);
+
+
+
+
+
+
+
+
+
+
